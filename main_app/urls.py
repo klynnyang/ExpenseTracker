0d0743c52d22546726cf477ef7de7d1c0d46b291
Lynn Yang <lynnyangnc@gmail.com>
2021-08-25 13:30:33 -0400

    email

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.urls import path
 from . import views
 
@@ -5,4 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
     path('user/', views.user, name='user'),
+    path('email/', views.send_email, name='email'),
+    path('budget/', views.budget_index, name='budget'),
 ]
