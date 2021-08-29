00449c40c0d2d0d147f0399da6447167003aef95
Lynn Yang <lynnyangnc@gmail.com>
2021-08-29 11:16:06 -0400

    change user login and signup page interface

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -88,7 +88,12 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
 
