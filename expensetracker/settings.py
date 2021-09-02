1bcbfe094c0d90366b568fb81c0b4c2cc1692b80
robertkoszegi <robkoszegi@gmail.com>
2021-09-02 16:39:03 -0400

    config deployment

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -15,6 +15,8 @@ import os
 import environ
 environ.Env()
 environ.Env.read_env()
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
